bb8f421677c30544b81dd7618c17c64fc61b6f60
Toby Clemson <tobyclemson@gmail.com>
2021-03-11 02:00:28 +0000

    Remove highlightjs-solidity.

--- src/ts/SyntaxHighlighting.ts
+++ src/ts/SyntaxHighlighting.ts
@@ -1,12 +1,9 @@
 import hljs from 'highlight.js';
 // @ts-ignore
-// import hljsDefineSolidity from 'highlightjs-solidity';
-// @ts-ignore
 import hljsLineNumbers from 'highlightjs-line-numbers2.js';
 
 class SyntaxHighlighting {
   initialize() {
-    // hljsDefineSolidity(hljs)
     hljsLineNumbers.init(hljs)
     hljs.highlightAll();
     // @ts-ignore
